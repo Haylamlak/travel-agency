a75923f8d3a0118079519f67a08698d209f86de5
haylamlak <gebreeyesusaylegn027@gmail.com>
2026-06-09 12:55:23 +0300

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,32 @@
+// const mysql = require("mysql2");
+// require("dotenv").config();
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306,
+
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// // TEST CONNECTION
+// db.getConnection((err, connection) => {
+//   if (err) {
+//     console.log("❌ DB connection error:");
+//     console.log(err);
+//   } else {
+//     console.log("✅ MySQL Pool Connected");
+//     connection.release();
+//   }
+// });
+
+// module.exports = db;
+
+
 const mysql = require("mysql2");
 require("dotenv").config();
 
@@ -7,21 +36,19 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
-
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-// TEST CONNECTION
-db.getConnection((err, connection) => {
-  if (err) {
-    console.log("❌ DB connection error:");
-    console.log(err);
-  } else {
-    console.log("✅ MySQL Pool Connected");
-    connection.release();
-  }
+db.on("error", (err) => {
+  console.log("❌ Pool error:", err);
 });
 
 module.exports = db;
+
+
+
+
+
+
