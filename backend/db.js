4b160432e0b2b65e23e852820ef2f4c314e23088
haylamlak <gebreeyesusaylegn027@gmail.com>
2026-06-09 12:59:26 +0300

    com

--- backend/db.js
+++ backend/db.js
@@ -26,26 +26,16 @@
 
 // module.exports = db;
 
-
-const mysql = require("mysql2");
-require("dotenv").config();
-
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 0
 });
-
-db.on("error", (err) => {
-  console.log("❌ Pool error:", err);
-});
-
-module.exports = db;
 
 
 
